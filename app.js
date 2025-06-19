a717e9b4dd3d338a3e5a974fdfb6d49b9d91d92f
DeringSalazar <desalazarsa@est.utn.ac.cr>
2025-06-19 14:35:17 -0600

    configuración

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000; 
 
 // Middleware para parsear JSON
 app.use(express.json());
